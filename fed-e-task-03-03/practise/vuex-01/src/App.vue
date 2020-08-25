1b6885244e32fd80e740cdb422803700aae1c7cb
mac <a1234@1234deMacBook-Pro.local>
2020-08-25 20:46:08 +0800

    vuex使用提交

--- fed-e-task-03-03/practise/vuex-01/src/App.vue
+++ fed-e-task-03-03/practise/vuex-01/src/App.vue
@@ -1,9 +1,37 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- state基本用法 -->
+      <h1>count: {{$store.state.count}}</h1>
+      <h1>message: {{$store.state.message}}</h1>
+      <!-- 使用计算属性简化获取 -->
+      <h1>count: {{count}}</h1>
+      <h1>message: {{message}}</h1>
+      <!-- 如果页面已经有相同属性，mapState使用对象方法传值 -->
+      <h1>count: {{num}}</h1>
+      <h1>message: {{msg}}</h1>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      count: 0,
+      message: ''
+    }
+  },
+  computed: {
+    // count () {
+    //   return this.$store.state.count
+    // },
+    // message () {
+    //   return this.$store.state.message
+    // }
+    // ...mapState(['count', 'message'])
+    ...mapState({ num: 'count', msg: 'message' })
+  }
+}
+</script>
