db16c8f791836b3481e1d8abac6ba12b95507fa7
mac <a1234@1234deMacBook-Pro.local>
2020-09-07 20:25:13 +0800

    ssr提交

---
+++ fed-e-task-03-03/practise/ssr/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <h1>{{ title }}</h1>
+        <!-- 循环 -->
+    <ul>
+        <li v-for="(item, index) in posts" :key="index">{{ item.title}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'ssr-demo',
+    // nuxt中特殊的钩子函数，专门用于获取服务端渲染的数据
+    async asyncData() {
+      const { data } = await axios({
+        method: 'GET',
+        url: 'http://localhost:3000/data.json'
+      })
+      // 这里返回的数据会和data () {}中的数据合并到一起给页面使用
+      return data
+    },
+  }
+</script>
