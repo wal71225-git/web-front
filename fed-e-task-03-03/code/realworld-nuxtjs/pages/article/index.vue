af7b99d86f54441df1c4c179cc981cd65bbc4315
mac <a1234@1234deMacBook-Pro.local>
2020-09-17 19:42:25 +0800

    例行提交

--- fed-e-task-03-03/code/realworld-nuxtjs/pages/article/index.vue
+++ fed-e-task-03-03/code/realworld-nuxtjs/pages/article/index.vue
@@ -2,26 +2,7 @@
   <div class="article-page">
     <div class="banner">
         <div class="container">
-        <h1>{{article.title}}</h1>
-        <div class="article-meta">
-            <a href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-            <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-            </div>
-            <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-            </button>
-            &nbsp;&nbsp;
-            <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-            </button>
-        </div>
-
+            <h1>{{article.title}}</h1>
         </div>
     </div>
     <div class="container page">
@@ -34,25 +15,7 @@
 
         <hr />
         <div class="article-actions">
-        <div class="article-meta">
-            <a href="profile.html"><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-            <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-            </div>
-
-            <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp;
-            Follow Eric Simons <span class="counter">(10)</span>
-            </button>
-            &nbsp;
-            <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp;
-            Favorite Post <span class="counter">(29)</span>
-            </button>
-        </div>
+                <article-meta :article="article"></article-meta>
         </div>
         <div class="row">
         <div class="col-xs-12 col-md-8 offset-md-2">
@@ -104,6 +67,7 @@
 </template>
 <script>
 import articleApi from '@/api/article'
+import articleMeta from '@/pages/components/article/article-meta.vue'
 export default {
   name: 'ArticleIndex',
   data () {
@@ -114,5 +78,13 @@ export default {
    const {data} = await articleApi.getArticle(params.slug)
    return data
   },
+  head() {
+    return {
+        title : `${this.article.title}`
+    }
+  },
+  components: {
+    articleMeta
+  }
 }
 </script>
