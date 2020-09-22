7695121a4c93c5acde7a9b3f6ec2bb956710ee51
mac <a1234@1234deMacBook-Pro.local>
2020-09-22 20:10:09 +0800

    进度条提交

---
+++ fed-e-task-03-04/practise/组件库开发/custom-component/src/examples/steps-test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <setps :active="active" :count="count"></setps>
+    <button @click="next">下一步</button>
+  </div>
+</template>
+<script>
+  import setps from '../components/steps'
+  export default {
+    name: 'stepsTest',
+    data() {
+      return {
+        active: 0, // 当前进行到第几步
+        count: 5 // 总共步骤
+      }
+    },
+    methods: {
+      next() {
+        this.active < this.count && this.active++
+      }
+    },
+    components: {
+      setps
+    }
+  }
+</script>
