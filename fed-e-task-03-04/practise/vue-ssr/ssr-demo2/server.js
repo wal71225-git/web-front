c5413cf7555e9a225c3022104df3bb3e2622fdc7
“e <liwei@sz-rst.com>
2020-10-18 22:40:54 +0800

    例行提交

--- fed-e-task-03-04/practise/vue-ssr/ssr-demo2/server.js
+++ fed-e-task-03-04/practise/vue-ssr/ssr-demo2/server.js
@@ -4,20 +4,36 @@
 const express = require('express')
 const Vue = require('vue')
 const fs = require('fs')
-// 引入打包后的服务端资源
-const serverBundle = require('./dist/vue-ssr-server-bundle.json') 
-const template = fs.readFileSync('./index.template.html', 'utf-8') 
-const clientManifest = require('./dist/vue-ssr-client-manifest.json')
 
-const render = require('vue-server-renderer').createBundleRenderer(serverBundle,{
+const isProd = process.env.NODE_ENV === 'production'
+
+// // 构建开发模式前
+// // 引入打包后的服务端资源
+// const serverBundle = require('./dist/vue-ssr-server-bundle.json') 
+// const template = fs.readFileSync('./index.template.html', 'utf-8') 
+// const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+// const render = require('vue-server-renderer').createBundleRenderer(serverBundle,{
+//    template, 
+//    clientManifest 
+//   })
+// 构建开发模式后
+let render
+if( isProd ) {
+  const serverBundle = require('./dist/vue-ssr-server-bundle.json') 
+  const template = fs.readFileSync('./index.template.html', 'utf-8') 
+  const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+  render = require('vue-server-renderer').createBundleRenderer(serverBundle,{
    template, 
    clientManifest 
   })
+} else {
+  // 开发模式 打包构建（客户端 + 服务端） -> 创建渲染器
+}
 
 const server = express()
 // 服务使用打包后的地址
-server.use('/dist', express.static('./dist'))
-
+ server.use('/dist', express.static('./dist'))
+ 
 server.get('/', (req, res) => {
   render.renderToString({
     title: '拉勾教育', 
@@ -27,7 +43,6 @@ server.get('/', (req, res) => {
       return res.status(500).end('Internal Server Error.')
     }
     res.setHeader('Content-Type', 'text/html; charset=utf8') 
-    console.log(html)
     res.end(html)
   })
 })
--- fed-e-task-03-04/practise/vue-ssr/ssr-demo2/src/App.vue
+++ fed-e-task-03-04/practise/vue-ssr/ssr-demo2/src/App.vue
@@ -1,11 +1,23 @@
 <template>
   <!--客户端渲染节点 -->
   <div id="app">
-    <h1>拉勾教育</h1>
+    <h1>{{message}}</h1>
+    <input type="text" v-model="message"/>
+    <button @click="test">点击测试</button>
   </div>
 </template>
 <script>
   export default {
-    name: 'App'
+    name: 'App',
+    data() {
+      return {
+        message: '拉勾教育'
+      }
+    },
+    methods: {
+      test() {
+        alert('点击测试')
+      } 
+    }
   }
 </script>
