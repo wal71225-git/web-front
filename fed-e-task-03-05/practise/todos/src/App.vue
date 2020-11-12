ef20c353480dab23ae6cf532cfe1de1636689105
mac <a1234@1234deMacBook-Pro.local>
2020-11-12 17:59:59 +0800

    创建vite项目

---
+++ fed-e-task-03-05/practise/todos/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="app">
+    <section  class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        autocomplete="off"
+        autofocus
+        v-model="input"
+        @keyup.enter="addTodo"
+        >
+    </header>
+    <section class="main" v-show="count">
+      <input id="toggle-all" class="toggle-all" v-model="allDone" type="checkbox">
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        <li
+          v-for="todo in filteredTodos"
+          :key="todo"
+          :class="{ editing: todo === editingTodo, completed: todo.completed }"
+        >
+          <div class="view">
+            <input class="toggle" type="checkbox" v-model="todo.completed">
+            <label @dblclick="editTodo(todo)">{{ todo.text }}</label>
+            <button class="destroy" @click="remove(todo)"></button>
+          </div>
+          <input
+            class="edit"
+            type="text"
+            v-editing-focus="todo === editingTodo"
+            v-model="todo.text"
+            @keyup.enter="doneEdit(todo)"
+            @blur="doneEdit(todo)"
+            @keyup.esc="cancelEdit(todo)"
+            >
+        </li>
+      </ul>
+    </section>
+    <footer class="footer" v-show="count">
+      <span class="todo-count">
+        <strong>{{ remainingCount }}</strong> {{ remainingCount > 1 ? 'items' : 'item' }} left
+      </span>
+      <ul class="filters">
+        <li><a href="#/all">All</a></li>
+        <li><a href="#/active">Active</a></li>
+        <li><a href="#/completed">Completed</a></li>
+      </ul>
+      <button class="clear-completed" @click="removeCompleted" v-show="count > remainingCount">
+        Clear completed
+      </button>
+    </footer>
+  </section>
+  <footer class="info">
+    <p>Double-click to edit a todo</p>
+    <!-- Remove the below line ↓ -->
+    <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+    <!-- Change this out with your name and url ↓ -->
+    <p>Created by <a href="https://www.lagou.com">教瘦</a></p>
+    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+  </footer>
+  </div>
+</template>
+
+<script>
+import './assets/index.css'
+import { ref, computed, onMounted, onUnmounted, watchEffect } from 'vue'
+// 1.添加待办事项
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
