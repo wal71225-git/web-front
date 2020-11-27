cd6a2e13206100e1b7308a309d2e4a20e1c300f1
mac <a1234@1234deMacBook-Pro.local>
2020-11-27 18:53:03 +0800

    feat:布局提交

---
+++ fed-e-task-03-06/edu-boss-fed/src/layout/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-container>
+      <el-aside width="200px">
+        <app-aside></app-aside>
+      </el-aside>
+    <el-container>
+        <el-header>
+          <app-header></app-header>
+        </el-header>
+        <el-main>
+          <!-- 子路由出口 -->
+          子路由
+          <router-view />
+        </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import Vue from 'vue'
+import AppAside from './components/app-aside'
+import AppHeader from './components/app-header'
+export default Vue.extend({
+  name: 'LayoutIndex',
+  components: {
+    AppAside,
+    AppHeader
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+  .el-container {
+  min-height: 100vh;
+  min-width: 980px;
+ }
+
+.el-aside {
+  background: #d3dce6;
+}
+
+.el-header {
+  background: #fff;
+}
+
+.el-main {
+  background: #e9eef3;
+}
+</style>
