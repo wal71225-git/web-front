250537944bd979e6eeede2813b93091b2c3647f7
mac <a1234@1234deMacBook-Pro.local>
2020-11-30 19:22:31 +0800

    例行提交

--- fed-e-task-03-06/edu-boss-fed/src/main.ts
+++ fed-e-task-03-06/edu-boss-fed/src/main.ts
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 加载全局样式
+import './styles/index.scss'
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
