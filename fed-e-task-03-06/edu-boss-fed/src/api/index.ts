90bf8c99e958ad62ab66d9bffe4356502ca99750
mac <a1234@1234deMacBook-Pro.local>
2020-12-13 17:55:34 +0800

    feat:角色添加完成

--- fed-e-task-03-06/edu-boss-fed/src/api/index.ts
+++ fed-e-task-03-06/edu-boss-fed/src/api/index.ts
@@ -1,8 +1,10 @@
 import * as user from './user'
 import * as menu from './menu'
 import * as resource from './resources'
+import * as role from './role'
 export default {
   user: user.default,
   menu: menu.default,
-  resource: resource.default
+  resource: resource.default,
+  role: role.default
 }
---
+++ fed-e-task-03-06/edu-boss-fed/src/views/role/components/addOrEdit.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-form ref="roleForm" :model="role" style="padding-right: 80px" size="120px">
+      <el-form-item label="角色编码" prop="code" :label-width="formLabelWidth">
+        <el-input v-model="role.code"  placeholder="角色编码"></el-input>
+      </el-form-item>
+      <el-form-item label="角色名称" prop="name" :label-width="formLabelWidth">
+        <el-input v-model="role.name" placeholder="角色名称"></el-input>
+      </el-form-item>
+      <el-form-item label="角色描述" prop="description" :label-width="formLabelWidth">
+        <el-input v-model="role.description" placeholder="角色名称"></el-input>
+      </el-form-item>
+      <el-form-item class="dialog-footer" style="text-align: right">
+        <el-button>取消</el-button>
+        <el-button type="primary" @click="onSubmit">确认</el-button>
+      </el-form-item>
+   </el-form>
+</template>
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'RoleAddOrEdit',
+  data() {
+    return {
+      role: {
+        code: '',
+        name: '',
+        description: ''
+      },
+      formLabelWidth: '120px'
+    }
+  },
+  methods: {
+    async onSubmit() {
+      await this.$api.role.saveOrUpdate(this.role)
+      this.$emit('success')
+    }
+  }
+})
+</script>
