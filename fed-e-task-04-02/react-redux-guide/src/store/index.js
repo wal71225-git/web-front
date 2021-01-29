575be6e2166ce40eba715668982f45abdafe6e4d
mac <a1234@1234deMacBook-Pro.local>
2021-01-29 17:04:59 +0800

    feat

--- fed-e-task-04-02/react-redux-guide/src/store/index.js
+++ fed-e-task-04-02/react-redux-guide/src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 // import reducer from '../store/reducers/count.reducer'
 import rootReducer from '../store/reducers/root.reducer'
+import logger from './middleware/logger'
 // store默认接收一个reducer函数
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 export default store
---
+++ fed-e-task-04-02/react-redux-guide/src/store/middleware/logger.js
@@ -0,0 +1,5 @@
+const logger = store => next => action => {
+  console.log('action', action)
+  next(action)
+}
+export default logger
