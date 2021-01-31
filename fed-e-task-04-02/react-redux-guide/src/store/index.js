bab472f93d52c8382c3736025ecdf9ae52bd5398
“e <liwei@sz-rst.com>
2021-01-31 13:57:35 +0800

    feat:使用redux-thunk中间件

--- fed-e-task-04-02/react-redux-guide/src/store/index.js
+++ fed-e-task-04-02/react-redux-guide/src/store/index.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from 'redux'
 // import reducer from '../store/reducers/count.reducer'
 import rootReducer from '../store/reducers/root.reducer'
 // import logger from './middleware/logger'
-import thunk from './middleware/thunk'
+// import thunk from './middleware/thunk'
+import thunk from 'redux-thunk'
 // store默认接收一个reducer函数
 const store = createStore(rootReducer, applyMiddleware(thunk))
 export default store
